f57d2a1ed9019d7cd5ebd10c1d79debb43c5ed02
Dylan Wheeler <dylaneugenewheeler@gmail.com>
2019-10-22 15:02:34 -0500

    DEfined readImageFromFile method,
    built sandbox for testing (allison)

---
+++ BayesNet/allison.py
@@ -0,0 +1,8 @@
+#THIS IS A TEST FILE, DO NOT INCLUDE IN FINAL IMPLEMENTATION
+
+#SANDBOX
+
+import imageHandler
+
+print("########## Starting allison.py ##########") 
+
---
+++ BayesNet/imageHandler.py
@@ -0,0 +1,36 @@
+"""
+Author:     Dylan E. Wheeler
+Date:       2019 10 22
+Course:     CSC 412 - Introduction to AI
+Prof.:      Dr. Bo Li
+
+This defines the methods necessary to read in an image file and 
+    store the drawn image into a case.
+
+    An image is a 28 by 28 grid of ASCII characters, 
+    henceforth refered to as pixels. Typically, 
+    each pixel can have one of three values:
+        ' ', '+', or '#'
+    each representing a white, grey, or black dot respectively.
+
+    The images should contain the data of a pixelated representation
+    of a digit, any of 0 through 9. The handler will use training data to 
+    prescribe a set of images and sort them by case (a case will exist for
+    each digit) and build a Bayesian Map for each case.
+
+    This Map will then be used to predict new images.
+
+"""
+
+#for square images, each image is this many pixels on each size
+imageSize = 28
+
+#read image
+#   this function reads an image in from a file and
+#   stores it into a 2D array of ASCII characters.
+def readImageFromFile(fFilename):
+    file = open(fFileName, r)
+
+    image = file.readlines(imageSize*imageSize) 
+
+    return image
